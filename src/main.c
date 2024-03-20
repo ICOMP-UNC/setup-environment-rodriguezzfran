07eb55130f4d5cb366fe5454cc84b04357485754
Franco <Francorod47@gmail.com>
2024-03-20 17:45:28 -0300

    Añadidos tests.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <greetings.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-    printf("Hello, OS II. Welcome to 2024 course! \n");
+#include "shared.h"
+#include "static.h"
+
+int main(int argc, char* argv[])
+{
+    printf("Hello, OS II. Welcome to 2024 course!\n");
     greetings();
+    printf("Calling shared lib from main program. I get '%d'\n",shared_lib_function("Hello, OS II"));
+    printf("Calling shared lib from main program. I get '%d'\n",static_lib_function("Bye OSII"));
     return 0;
 }
